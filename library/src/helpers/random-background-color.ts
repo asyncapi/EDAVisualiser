4e011840063d6d3b47827ad456e602f463ff4e26
Jonas Lagoni <jonas-lt@live.dk>
2022-04-22 12:09:30 +0200

    fix: setup for release

--- library/src/helpers/random-background-color.ts
+++ library/src/helpers/random-background-color.ts
@@ -1,5 +1,6 @@
 export default (stringInput: string) => {
   const stringUniqueHash = [...stringInput].reduce((acc, char) => {
+    // tslint:disable-next-line
     return char.charCodeAt(0) + ((acc << 5) - acc);
   }, 0);
   return `hsla(${stringUniqueHash % 360}, 95%, 35%, 0.5)`;
