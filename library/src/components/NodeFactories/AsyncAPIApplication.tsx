ce662060d14a5c0795167983b7d7baeffe5c63ab
Jonas Lagoni <jonas-lt@live.dk>
2022-05-02 19:56:29 +0200

    fix: lint error

--- library/src/components/NodeFactories/AsyncAPIApplication.tsx
+++ library/src/components/NodeFactories/AsyncAPIApplication.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { ApplicationLicenseData, ApplicationNodeData, ApplicationServerData, MessageData } from '../../types';
+import {
+  ApplicationLicenseData,
+  ApplicationNodeData,
+  ApplicationServerData,
+  MessageData,
+} from '../../types';
 import { Application } from './Application';
 import { Outgoing } from './Outgoing';
-import {AsyncAPIDocument} from '@asyncapi/parser';
+import { AsyncAPIDocument } from '@asyncapi/parser';
 import { Incoming } from './Incoming';
 type InternalApplicationProps = {
   internal?: {
     addElementCallback?: any;
   };
-  document: AsyncAPIDocument
+  document: AsyncAPIDocument;
 };
 
 type ApplicationProps = ApplicationNodeData & InternalApplicationProps;
@@ -21,55 +26,82 @@ type ApplicationProps = ApplicationNodeData & InternalApplicationProps;
 export const AsyncAPIApplication: React.FunctionComponent<ApplicationProps> = props => {
   const outgoingNodes = [];
   const incomingNodes = [];
-  for (const [channelPath, channel] of Object.entries(props.document.channels())) {
-    if(channel.hasPublish()) {
+  for (const [channelPath, channel] of Object.entries(
+    props.document.channels(),
+  )) {
+    if (channel.hasPublish()) {
       const channelId = props.document.info().title() + channelPath;
-      const messages: MessageData[] = channel.publish().messages().map((message) => {
-        return {title: message.name() || 'Unknown'}
-      });
-      incomingNodes.push(<Incoming 
-        internal={{addElementCallback: props.internal?.addElementCallback}}
-        channel={channelPath} 
-        description={channel.description() || 'No description'} 
-        id={channelId} 
-        messages={messages}></Incoming>);
+      const messages: MessageData[] = channel
+        .publish()
+        .messages()
+        .map(message => {
+          return { title: message.name() || 'Unknown' };
+        });
+      incomingNodes.push(
+        <Incoming
+          internal={{ addElementCallback: props.internal?.addElementCallback }}
+          channel={channelPath}
+          description={channel.description() || 'No description'}
+          id={channelId}
+          messages={messages}
+        />,
+      );
     } else if (channel.hasSubscribe()) {
       const channelId = props.document.info().title() + channelPath;
-      const messages: MessageData[] = channel.subscribe().messages().map((message) => {
-        return {title: message.name() || 'Unknown'}
-      });
-      outgoingNodes.push(<Outgoing 
-        internal={{addElementCallback: props.internal?.addElementCallback}} 
-        channel={channelPath} 
-        description={channel.description() || 'No description'} 
-        id={channelId} 
-        messages={messages}></Outgoing>);
+      const messages: MessageData[] = channel
+        .subscribe()
+        .messages()
+        .map(message => {
+          return { title: message.name() || 'Unknown' };
+        });
+      outgoingNodes.push(
+        <Outgoing
+          internal={{ addElementCallback: props.internal?.addElementCallback }}
+          channel={channelPath}
+          description={channel.description() || 'No description'}
+          id={channelId}
+          messages={messages}
+        />,
+      );
     }
   }
   const license: ApplicationLicenseData = {
-    name: props.document.info()?.license()?.name() || 'Not defined',
-    url: props.document.info()?.license()?.url() || 'Not defined'
-  }
-  const servers: ApplicationServerData[] = Object.entries(props.document.servers()).map(([serverId, server]) => {
+    name:
+      props.document
+        .info()
+        ?.license()
+        ?.name() || 'Not defined',
+    url:
+      props.document
+        .info()
+        ?.license()
+        ?.url() || 'Not defined',
+  };
+  const servers: ApplicationServerData[] = Object.entries(
+    props.document.servers(),
+  ).map(([serverId, server]) => {
     return {
       description: server.description() || 'No description',
       name: serverId,
       protocol: server.protocol() || 'No protocol',
       protocolVersion: server.protocolVersion() || 'No protocol version',
-      url: server.url()
-    }
-  })
-  return <Application
-    internal={{addElementCallback: props.internal?.addElementCallback}} 
-    defaultContentType={props.document.defaultContentType() || 'Undefined'} 
-    description={props.document.info().description() || 'No description'}
-    id={props.document.info().title()} 
-    license={license} 
-    externalDocs={props.document.externalDocs()?.url() || 'No external docs'} 
-    servers={servers} 
-    title={props.document.info().title()} 
-    version={props.document.info().version()}>
-    {incomingNodes}
-    {outgoingNodes}
-  </Application>
+      url: server.url(),
+    };
+  });
+  return (
+    <Application
+      internal={{ addElementCallback: props.internal?.addElementCallback }}
+      defaultContentType={props.document.defaultContentType() || 'Undefined'}
+      description={props.document.info().description() || 'No description'}
+      id={props.document.info().title()}
+      license={license}
+      externalDocs={props.document.externalDocs()?.url() || 'No external docs'}
+      servers={servers}
+      title={props.document.info().title()}
+      version={props.document.info().version()}
+    >
+      {incomingNodes}
+      {outgoingNodes}
+    </Application>
+  );
 };
