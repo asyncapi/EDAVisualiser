56e7829fad9241594dd9e25ee31af51a09e31f57
Shashwat Pratap Singh <114943221+ShashwatPS@users.noreply.github.com>
2023-10-04 17:51:06 +0530

    Removed the Extra Line

--- library/src/components/layouts/CircleLayout.tsx
+++ library/src/components/layouts/CircleLayout.tsx
@@ -18,8 +18,6 @@ function calculateLayout(
   elements: Node[],
   passedOptions: Partial<CircleLayoutOptions> = defaultOptions,
 ): FlowElement[] {
- 
-
   const eachDegree = 360 / elements.length;
   let nextDegree = 0;
   const r = elements.length * 100;
